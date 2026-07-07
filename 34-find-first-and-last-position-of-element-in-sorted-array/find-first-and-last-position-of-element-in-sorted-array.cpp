b71344513899c5a6ed46ba7565d21e091162ee72
sanika2506 <shindesanika0625@gmail.com>
2026-07-07 10:36:44 +0530

    Time: 0 ms (100.00%) | Memory: 17.4 MB (89.51%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,16 +1,59 @@
 class Solution {
 public:
-    vector<int> searchRange(vector<int>& nums, int target) {
-        int first=-1;
-        int last = -1;
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]==target){
-                if(first==-1){
-                    first = i;
-                }
-                last = i;
+    int firstOcc(vector<int> &arr ,int n,int k){
+        int low = 0;
+        int high = n-1;
+        int first = -1;
+        while(low<=high){
+            int mid = (low+high)/2;
+            if(arr[mid]==k){
+                first = mid;
+                high = mid-1;
+            }else if(arr[mid]<k){
+                low = mid +1;
+            }else{
+                high = mid - 1 ;
             }
         }
+        return first;
+    }
+    int lastOcc(vector<int> &arr ,int n,int k){
+        int low = 0;
+        int high = n-1;
+        int last = -1;
+        while(low<=high){
+            int mid = (low+high)/2;
+            if(arr[mid]==k){
+                last = mid;
+                low = mid+1;
+            }else if(arr[mid]<k){
+                low = mid +1;
+            }else{
+                high = mid - 1 ;
+            }
+        }
+        return last;
+    }
+    vector<int> searchRange(vector<int>& nums, int target) {
+        //time complexity of this is o(n)
+        // int first=-1;
+        // int last = -1;
+        // for(int i=0;i<nums.size();i++){
+        //     if(nums[i]==target){
+        //         if(first==-1){
+        //             first = i;
+        //         }
+        //         last = i;
+        //     }
+        // }
+        // return {first,last};
+
+        //this is binary approch time complexity is o(nlogn) but not accepted in leetcode
+        int first = firstOcc(nums,nums.size(),target);
+        if(first == -1){
+            return {-1,-1};
+        }
+        int last = lastOcc(nums,nums.size(),target);
         return {first,last};
     }
 };
