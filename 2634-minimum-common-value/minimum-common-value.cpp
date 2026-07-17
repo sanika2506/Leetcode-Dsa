e8e0bab237f2f69b88630428a3a9e2712ff77b8c
sanika2506 <shindesanika0625@gmail.com>
2026-07-17 15:49:37 +0530

    Time: 0 ms (100.00%) | Memory: 54.6 MB (56.05%) - LeetSync

---
+++ 2634-minimum-common-value/minimum-common-value.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int getCommon(vector<int>& nums1, vector<int>& nums2) {
+        //bruteforce solution
+        // set<int>ans;
+        // for(int i=0;i<nums1.size();i++){
+        //     for(int j=0;j<nums2.size();j++){
+        //         if(nums1[i]==nums2[j]){
+        //             ans.insert(nums1[i]);
+        //         }
+        //     }
+        // }
+        // return *ans.begin();
+        int  i= 0,j=0;
+        while(i<nums1.size() && j<nums2.size()){
+            if(nums1[i]==nums2[j]){
+                return nums1[i];
+                break;
+            }else if(nums1[i]<nums2[j]){
+                i++;
+            }else{
+                j++;
+            }
+        }
+        return -1;
+    }
+};
