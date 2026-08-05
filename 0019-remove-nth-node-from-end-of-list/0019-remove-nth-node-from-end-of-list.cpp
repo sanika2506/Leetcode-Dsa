3fe3dcccd97293e088b1222b0ed8f40dcf4b5624
sanika2506 <shindesanika0625@gmail.com>
2026-08-05 23:32:31 +0530

    Time: 0 ms (100%), Space: 15.1 MB (27.07%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
@@ -11,29 +11,47 @@
 class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
+        // //bruteforce solution
+        // int cnt =0;
+        // ListNode* temp = head;
+        // while(temp!=NULL){
+        //     temp = temp->next;
+        //     cnt++;
+        // }
+        // if(cnt==n){
+        //     ListNode* newHead = head->next;
+        //     delete head;
+        //     return newHead;
+        // }
+        // int res = cnt-n;
+        // temp = head;
+        // while(temp!=NULL){
+        //     res--;
+        //     if(res==0){
+        //         break;
+        //     }
+        //     temp = temp->next;
+        // }
+        // ListNode* delNode = temp;
+        // temp->next = temp->next->next;
+        // return head;
+
         //optimal solution
-        int cnt =0;
-        ListNode* temp = head;
-        while(temp!=NULL){
-            temp = temp->next;
-            cnt++;
+        ListNode* fast = head;
+        for(int i=0;i<n;i++){
+            fast = fast->next;
         }
-        if(cnt==n){
-            ListNode* newHead = head->next;
-            delete head;
-            return newHead;
+        if(fast==NULL){
+            return head->next;
         }
-        int res = cnt-n;
-        temp = head;
-        while(temp!=NULL){
-            res--;
-            if(res==0){
-                break;
-            }
-            temp = temp->next;
+        ListNode* slow = head;
+        while(fast->next!=NULL){
+            slow = slow->next;
+            fast = fast->next;
         }
-        ListNode* delNode = temp;
-        temp->next = temp->next->next;
+        ListNode* delNode = slow->next;
+        slow->next = slow->next->next;
+        delete delNode;
         return head;
     }
 };
