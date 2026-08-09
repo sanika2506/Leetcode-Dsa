c4ca74141a35b29922ec8c255bbc4f021c770bca
sanika2506 <shindesanika0625@gmail.com>
2026-08-09 13:45:08 +0530

    Time: 6 ms (91.97%), Space: 11.8 MB (54.59%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -9,14 +9,27 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        map<ListNode*,int> mpp;
-        ListNode* temp = head;
-        while(temp!=NULL){
-            if(mpp.find(temp)!=mpp.end()){
+        ////bruteforce solution using the hashing
+        // map<ListNode*,int> mpp;
+        // ListNode* temp = head;
+        // while(temp!=NULL){
+        //     if(mpp.find(temp)!=mpp.end()){
+        //         return true;
+        //     }
+        //     mpp[temp]=1;
+        //     temp=temp->next;
+        // }
+        // return false;
+
+        //optimal solution using tortoise & hare approach
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while(fast!=NULL && fast->next!=NULL){
+            slow = slow->next;
+            fast = fast->next->next;
+            if(slow == fast){
                 return true;
             }
-            mpp[temp]=1;
-            temp=temp->next;
         }
         return false;
     }
