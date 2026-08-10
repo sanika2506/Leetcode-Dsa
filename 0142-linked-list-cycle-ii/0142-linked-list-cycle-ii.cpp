975cb8d0fa430a396f1e481ee1bcaf9dc88c26ad
sanika2506 <shindesanika0625@gmail.com>
2026-08-10 21:25:01 +0530

    Time: 7 ms (66.57%), Space: 11.3 MB (86.66%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
@@ -9,17 +9,39 @@
 class Solution {
 public:
     ListNode *detectCycle(ListNode *head) {
+        //bruteforce solution using hashmap
+        // if(head==NULL || head->next ==NULL){
+        //     return NULL;
+        // }
+        // map<ListNode*,int> mpp;
+        // ListNode* temp = head;
+        // while(temp!=NULL){
+        //     if(mpp.find(temp)!=mpp.end()){
+        //         return temp;
+        //     }
+        //     mpp[temp]=1;
+        //     temp = temp->next;
+        // }
+        // return NULL;
+
+
+        //optimal solution using fast and slow pointer
+        ListNode* slow = head;
+        ListNode* fast = head;
         if(head==NULL || head->next ==NULL){
             return NULL;
         }
-        map<ListNode*,int> mpp;
-        ListNode* temp = head;
-        while(temp!=NULL){
-            if(mpp.find(temp)!=mpp.end()){
-                return temp;
+        while(fast!=NULL && fast->next!=NULL){
+            slow=slow->next;
+            fast = fast->next->next;
+            if(slow==fast){
+                slow = head;
+                while(slow!=fast){
+                    slow = slow->next;
+                    fast = fast->next;
+                }
+                return slow;
             }
-            mpp[temp]=1;
-            temp = temp->next;
         }
         return NULL;
     }
