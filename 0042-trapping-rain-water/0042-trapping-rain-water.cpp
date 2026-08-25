f85f0c96d892b86d45753e7805b906c876bfdb10
sanika2506 <shindesanika0625@gmail.com>
2026-08-25 21:45:14 +0530

    Time: 0 ms (100%), Space: 26 MB (56.46%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,23 +1,44 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
-        int n = height.size();
-        vector<int>prefix(n);
-        vector<int>suffix(n);
-        prefix[0]=height[0];
-        for(int i=1;i<n;i++){
-            prefix[i]=max(prefix[i-1],height[i]);
-        }
-        suffix[n-1]=height[n-1];
-        for(int i=n-2;i>0;i--){
-            suffix[i]=max(suffix[i+1],height[i]);
-        }
-        int total = 0;
-        for(int i=0;i<n;i++){
-            int leftMax = prefix[i];
-            int rightMax = suffix[i];
-            if(height[i]<leftMax && height[i]<rightMax){
-                total += min(leftMax,rightMax)-height[i];
+        // int n = height.size();
+        // vector<int>prefix(n);
+        // vector<int>suffix(n);
+        // prefix[0]=height[0];
+        // for(int i=1;i<n;i++){
+        //     prefix[i]=max(prefix[i-1],height[i]);
+        // }
+        // suffix[n-1]=height[n-1];
+        // for(int i=n-2;i>0;i--){
+        //     suffix[i]=max(suffix[i+1],height[i]);
+        // }
+        // int total = 0;
+        // for(int i=0;i<n;i++){
+        //     int leftMax = prefix[i];
+        //     int rightMax = suffix[i];
+        //     if(height[i]<leftMax && height[i]<rightMax){
+        //         total += min(leftMax,rightMax)-height[i];
+        //     }
+        // }
+        // return total;
+        int lMax =0,rMax =0, total = 0;
+        int l =0;
+        int r = height.size()-1;
+        while(l<r){
+            if(height[l]<height[r]){
+                if(lMax>height[l]){
+                    total+=lMax-height[l];
+                }else{
+                    lMax = height[l];
+                }
+                l=l+1;
+            }else{
+                if(rMax>height[r]){
+                    total+=rMax - height[r];
+                }else{
+                    rMax = height[r];
+                }
+                r = r-1;
             }
         }
         return total;
