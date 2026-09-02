f3be8caa63654aa8b5749a2439f7221449cfd2ed
sanika2506 <shindesanika0625@gmail.com>
2026-09-02 18:51:00 +0530

    Time: 73 ms (50.24%), Space: 111.2 MB (62.93%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,14 +1,26 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        map<int,int> mpp;
-        for(int i=0;i<nums.size();i++){
-            mpp[nums[i]]++;
-        }
-        for(auto it : mpp){
-            if(it.second>1){
+        // using hashing approach 
+
+        // map<int,int> mpp;
+        // for(int i=0;i<nums.size();i++){
+        //     mpp[nums[i]]++;
+        // }
+        // for(auto it : mpp){
+        //     if(it.second>1){
+        //         return true;
+        //     }
+        // }
+        // return false;
+
+        //using unordered set optimal approach
+        unordered_set<int> st;
+        for(int x : nums){
+            if(st.find(x)!=st.end()){
                 return true;
             }
+            st.insert(x);
         }
         return false;
     }
