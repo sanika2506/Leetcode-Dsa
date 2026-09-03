6be46ae36aa0e5a716ddf497465c423314a1de4e
sanika2506 <shindesanika0625@gmail.com>
2026-09-03 17:54:50 +0530

    Time: 228 ms (5.06%) | Memory: 23.9 MB (55.89%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                if(nums[i]==0){
+                    swap(nums[i],nums[j]);
+                }
+            }
+        }
+    }
+};
